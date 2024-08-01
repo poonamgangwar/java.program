4254ad9d04ab12b8cff1657affd0b2054f7405d3
poonam kumari <53055667+poonamgangwar@users.noreply.github.com>
2024-08-01 19:16:23 +0530

    Add files via upload

--- Strings.java
+++ Strings.java
@@ -3,19 +3,32 @@ import java.util.*;
 
     public class Strings {
         public static void main(String args[]) {
-            Scanner sc = new Scanner (System.in);
-            String str = sc.next();
-            String result = "";
+            String name1 = "Tony";
+            String name2 = "Tony";
 
 
-            for(int i=0; i<str.length(); i++) {
-                if(str.charAt(i) == 'e') {
-                    result += 'i';
-                } else {
-                    result += str.charAt(i);
-                }
+            if(name1.equals(name2)) {
+                System.out.println("They are the same string");
+            } else {
+                System.out.println("They are different strings");
             }
 
-            System.out.println(result);
+
+            //DO NOT USE == to check for string equality
+            //Gives correct answer here
+            if(name1 == name2) {
+                System.out.println("They are the same string");
+            } else {
+                System.out.println("They are different strings");
+            }
+
+
+            //Gives incorrect answer here
+            if(new String("Tony") == new String("Tony")) {
+                System.out.println("They are the same string");
+            } else {
+                System.out.println("They are different strings");
+            }
+
         }
     }
